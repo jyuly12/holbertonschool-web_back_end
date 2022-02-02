60f36fa5953313e830aa21a118406a79a00c4a65
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-01 19:26:59 -0500

    A little fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,6 +8,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
+    """
+    """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    """
     delays: List[float] = []
     all_delays: List[float] = []
     for i in range(n):
