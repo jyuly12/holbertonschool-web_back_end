83cbf9a3b5cf967c745cf29e500c85b509e3fc1f
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-15 18:52:20 -0500

    Delete pychache and complete task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -14,12 +14,17 @@ class Auth():
         """
         if path is None:
             return True
-        if (excluded_paths is None) or (excluded_paths == ""):
+
+        if (excluded_paths is None) or not len(excluded_paths):
             return True
-        for item in excluded_paths:
-            if item == path:
-                return False
-        return False
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ public method
