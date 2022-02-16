b94f4041f10164cd347108dbe810a939e2c829c2
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-15 22:41:08 -0500

    Delete cache files and update basic_auth file

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Authentication process
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        header = authorization_header.split(" ")
+        if header[0] != "Basic":
+            return None
+        else:
+            return header[1]
