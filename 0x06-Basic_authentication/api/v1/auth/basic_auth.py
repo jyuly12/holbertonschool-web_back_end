03f4a1ce0ebafaf0e519b739c84835af7e67d634
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-15 22:57:16 -0500

    Add a new method to basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Basic authetication definition
 """
+
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,23 @@ class BasicAuth(Auth):
             return None
         else:
             return header[1]
+
+    def decode_base64_authorization_header(
+                                            self,
+                                            base64_authorization_header: str
+                                            ) -> str:
+        """ Decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            encode_value = base64_authorization_header.encode('utf-8')
+            decode_value = base64.b64decode(encode_value)
+            decoded_Value = decode_value.decode('utf-8')
+            return decoded_Value
+        except Exception:
+            return None
