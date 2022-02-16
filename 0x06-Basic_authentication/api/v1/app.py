f12193696ac4a53abf6e44c0230b62965183dba9
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-15 20:30:09 -0500

    update auth.py file and app.py file

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -6,12 +6,35 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth = Auth()
+
+
+@app.before_request
+def request_filter() -> None:
+    """ Registers a function to run before each request.
+    """
+    excluded = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+        ]
+
+    if auth is None:
+        pass
+    else:
+        if auth.require_auth(request.path, excluded):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
