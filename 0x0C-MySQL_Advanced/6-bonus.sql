84a61c8a28499f9881fc496228a5376215f97027
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-03-17 22:02:04 -0500

    procedure example

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,20 @@
 -- Create a stored procedure AddBonus.
 -- That adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
 
 DELIMITER $$
 
-CREATE PROCEDURE AddBonus(
+CREATE PROCEDURE AddBonus (
     IN user_id INT, 
     IN project_name VARCHAR(255),
-    IN score FLOAT)
+    IN score FLOAT
+)
 BEGIN
     INSERT INTO projects (name)
     SELECT project_name FROM DUAL
 
-    WHERE NOT EXISTS (
-        SELECT * FROM projects 
-        WHERE name = project_name);
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
-END &&
-DELIMITER ;
+END;
+&&
