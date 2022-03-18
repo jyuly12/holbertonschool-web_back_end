027c21c10b083687c62e125f533b89899d407b55
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-03-17 20:47:03 -0500

    Add examples of trigger

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Decreases the quantity of an item after adding a new order.
+DELIMITER 
+$$
+CREATE TRIGGER
+reset_attribute
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email <> NEW.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+$$
