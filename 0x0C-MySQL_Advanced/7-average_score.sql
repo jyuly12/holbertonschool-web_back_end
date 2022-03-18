667c17e8f71577e785e14b8e4cd7ec14107acb7d
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-03-17 22:23:52 -0500

    procedure example

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,11 @@
+-- Creates a stored procedure ComputeAverageScoreForUser
+-- Computes and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    INSERT INTO users ()
+END;
+&&
