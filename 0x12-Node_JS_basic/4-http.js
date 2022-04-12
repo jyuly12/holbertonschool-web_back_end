5b2c143ac3e209f9cf013313400ddc836ba395bf
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-04-11 22:42:43 -0500

    Add files

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 // create a server object:
-http.createServer((req, app) => {
+const app = http.createServer((req, app) => {
   app.write('Hello Holberton School!'); // write a response to the client
   app.end(); // end the response
 }).listen(1245); // the server object listens on port 1245
+
+module.exports = app;
