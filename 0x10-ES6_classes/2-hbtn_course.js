99b2fdf3ac0336416737a687668957de71f63ba8
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-04-28 23:20:10 -0500

    Add Files

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-export default class ClassRoom {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,4 +1,4 @@
-export default class ClassRoom {
+export default class Currency {
   constructor(code, name) {
     this._code = code;
     this._name = name;
